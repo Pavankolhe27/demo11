fe203eb92fe92396ff93585e0e31202044d6b325
Pavankolhe27 <101618136+Pavankolhe27@users.noreply.github.com>
2022-03-15 12:08:20 +0530

    Add files via upload

---
+++ Fun2.java
@@ -0,0 +1,10 @@
+class Fun{
+static void add(int n1,int n2)//defination
+{ int result=n1+n2;
+System.out.println(" sum is "+result);
+}
+public static void main (String ar[])
+{
+   add(5,7);//calling
+}
+}
